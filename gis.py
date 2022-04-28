779d508060d672d48af7893e4ad578c6c5ce1da9
Xavier Rivera <riverax34@gmail.com>
2022-04-28 18:13:13 -0400

    added import and test code

--- gis.py
+++ gis.py
@@ -1,6 +1,13 @@
+import numpy as np
+
+
 class gis:
     atrOptions = {"name": 1, "state": 2, "latitude": 3,
                   "longitude": 4, "population": 5}
+    currentlySelected = []
+    dt = np.dtype([("name", str), ("state", str), ("coordinates", [
+        ("latitude", np.int64), ("longitude", np.int64)]), ("population", np.int64)])
+
     # Ctor
 
     def __init__(self):
@@ -14,14 +21,51 @@ class gis:
         (isAnAtr, atrValue) = self.isAtrValid(attribute)
         isALowerBound = self.isBoundValid(atrValue, lowerBound)
         isAnUpperBound = self.isBoundValid(atrValue, upperBound)
+
+        if (isAnAtr, isALowerBound, isAnUpperBound):
+            pass
         pass
 
+    ######################################
+    #Checks if attribute is a valid input#
+    ######################################
     def isAtrValid(self, attribute):
         if attribute in self.options.keys():
             return (True, self.atrOptions[attribute])
         return (False, -1)
 
-    def isBoundValid(self, atrValue, lowerBound):
-        if atrValue == 1 or atrValue == 2 & & typeof(lowerBound) == str:
+    ################################################
+    #Checks if upper or lower bound are right types#
+    ################################################
+    def isBoundValid(self, atrValue, boundValue):
+        if atrValue in range(1, 3) and type(boundValue) == str:
+            return True
+        if atrValue in range(3, 6) and type(boundValue) == int:
             return True
         return False
+
+    ###################################
+    #select all cities in gis.dat file#
+    ###################################
+    def selectAllCities(self):
+        self.currentlySelected = np.fromfile("gis.dat", dtype=self.dt)
+
+    #######################################
+    #unselect all cities in current filter#
+    #######################################
+
+    def unselectAllCities(self):
+        self.currentlySelected = []
+
+    def printCurrent(self):
+        for i in range(len(self.currentlySelected)):
+            print("Current line: %d \n %s" % (i, self.currentlySelected[i]))
+
+
+def main():
+    test = gis()
+    test.selectAllCities()
+    test.printCurrent()
+
+
+main()
